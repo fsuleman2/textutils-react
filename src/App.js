3132ec77ca44131d8d34601544b36d7b75575a43
fsuleman2 <sydsulmn7@gmail.com>
2022-01-17 23:27:51 +0530

    video 10 dark mode completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 function App() {
   return (
     <>
       <Navbar title="TextUtils"/>
       <div className="container my-3">
-      <TextForm heading="Enter the text to analyze"/>
+      {/* <TextForm heading="Enter the text to analyze"/> */}
+      <About/>
       </div>
     </>
   );
